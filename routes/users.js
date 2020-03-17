80bb300b4eb27f2712100253aeedaecb68c2662d
Alexey Shiryaev <ashiryaev67@yandex.ru>
2020-03-17 15:35:39 +0300

    Исправил: путь к файлу json указан при помощи модуля path, также использован модуль fs с промисами для чтения файла json, прописал комментарий-игнорирования eslint

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,58 @@
 const router = require('express').Router();
-const users = require('../data/users.json');
+const path = require('path');
+const fs = require('fs').promises;
 
 router.get('/users', (req, res) => {
-  res.send(users);
+  fs.readFile(path.join(__dirname, '../data/users.json'), { encoding: 'utf8' })
+    .then((data) => {
+      if (data !== undefined) {
+        res.send(data);
+      } else {
+        res.send({ message: 'Невозможно прочитать .json файл' });
+      }
+    })
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.log(err);
+    });
 });
 router.get('/users/:id', (req, res) => {
-  const index = users.findIndex((data) => data._id === req.params.id);
-  if (index < 0) {
-    res.status(404).send({ message: 'Нет пользователя с таким id' });
-  } else {
-    res.send(users[index]);
-  }
+  fs.readFile(path.join(__dirname, '../data/users.json'), { encoding: 'utf8' })
+    .then((data) => {
+      if (data !== undefined) {
+        for (let i = 0; i < data.length; i += 1) {
+          // eslint-disable-next-line no-underscore-dangle
+          if (JSON.parse(data)[i]._id === req.params.id) {
+            res.send(JSON.parse(data)[i]);
+            return;
+          }
+        }
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
+      } else {
+        res.send({ message: 'Невозможно прочитать .json файл' });
+      }
+    })
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.log(err);
+    });
 });
 module.exports = router;
+
+// const usersRouter = require('express').Router();
+// const users = require('../data/users.json');
+
+// usersRouter.get('/', (req, res) => {
+//   res.send(users);
+// });
+
+// usersRouter.get('/:id', (req, res) => {
+//   const indexUser = users.findIndex((item) => item._id === req.params.id);
+//   if (indexUser < 0) {
+//     res.status(404).send({ message: 'Нет пользователя с таким id' });
+//   } else {
+//     res.send(users[indexUser]);
+//   }
+// });
+
+// module.exports = usersRouter;
