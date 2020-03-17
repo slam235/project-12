6b420f39f5aa6524d79008d415e9a1f7a3d84c53
Alexey Shiryaev <ashiryaev67@yandex.ru>
2020-03-17 15:38:19 +0300

    Исправил: путь к файлу json указан при помощи модуля path, также использован модуль fs с промисами для чтения файла json, прописал комментарий-игнорирования eslint

--- routes/users.js
+++ routes/users.js
@@ -38,21 +38,3 @@ router.get('/users/:id', (req, res) => {
     });
 });
 module.exports = router;
-
-// const usersRouter = require('express').Router();
-// const users = require('../data/users.json');
-
-// usersRouter.get('/', (req, res) => {
-//   res.send(users);
-// });
-
-// usersRouter.get('/:id', (req, res) => {
-//   const indexUser = users.findIndex((item) => item._id === req.params.id);
-//   if (indexUser < 0) {
-//     res.status(404).send({ message: 'Нет пользователя с таким id' });
-//   } else {
-//     res.send(users[indexUser]);
-//   }
-// });
-
-// module.exports = usersRouter;
